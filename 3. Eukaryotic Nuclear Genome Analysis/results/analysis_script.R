97631e7877279d5a0efd22a34190aed434cd40ec
Aleks <54845024+AleksandraLaura@users.noreply.github.com>
2024-01-30 12:46:52 +0100

    Update analysis_script.R

--- 3. Eukaryotic Nuclear Genome Analysis/results/analysis_script.R
+++ 3. Eukaryotic Nuclear Genome Analysis/results/analysis_script.R
@@ -17,14 +17,18 @@ df <- read_csv(input_file)
 # subsetting the table for Eukaryotes using grepl and filter for taxonomic classification at genus level with 10 reads or more classified
 dt <- df %>% filter(N_reads >= 10, grepl("Eukaryota", tax_path), grepl("\\bgenus\\b", tax_rank))
 
-# Plot taxa and their amount of Damage and the significance with size = number of reads for all 4 files
-pdf(file = "Damage_significance_Eukatota.pdf", width = 6, height = 5)  
+# subsetting the dataframe and keeping only the 95% similarity threshold profiles
+subset_data1 <- subset(dt, !grepl("ERR", sample))
+
+
+# plot taxa and their amount of Damage and the significance with size = number of reads for 95 similarity files
+pdf(file = "Damage_significance_Eukatota95sim.pdf", width = 6, height = 5)
 ggplot() +
-  geom_text(data = dt, aes(x = MAP_damage, y = MAP_significance, label = tax_name), size = 3, vjust = -1, hjust = 0) +
-  geom_point(data = dt, aes(x = MAP_damage, y = MAP_significance, fill = "grey", size = N_reads), alpha = 0.5) +
+  geom_text(data = subset_data1, aes(x = MAP_damage, y = MAP_significance, label = tax_name), size = 3, vjust = -1, hjust = 0) +
+  geom_point(data = subset_data1, aes(x = MAP_damage, y = MAP_significance, colour = sample, size = N_reads),alpha = 0.8) +
+  scale_colour_manual(values = c("#FB8379","#5CCBCF"), name = "Sample") +  # Add your chosen colors
   ggtitle("") +
   labs(col = "depths", fill = "Taxa") +
-  facet_wrap(~sample) +
   theme_test()
 dev.off()
 
@@ -50,12 +54,13 @@ ggplot() +
   theme_test()
 dev.off()
 
-# Plot mean readlength distribution for the 95 similarity files as a density plot
+
+# plot mean readlength distribution for the 95 similarity files as a density plot
 pdf(file = "density_meanlength.pdf", width = 5, height = 3)
-ggplot(subset_data1, aes(x = mean_L, fill = sample)) +
+ggplot(dt, aes(x = mean_L, fill = sample)) +
   geom_density(alpha = 0.8) +
-  scale_fill_manual(values = c("#92c5de", "darkgrey"), name = "Sample") +
-  xlim(30, 450) + ylim(0, 0.011) + theme_test()
+  scale_fill_manual(values = c("gray", "#F7D167", "#72D8A6", "#5CCBCF", "#FB8379"), name = "Sample") +  # Add your chosen colors
+  xlim(25,450) +  ylim(0,0.075) + theme_test()
 dev.off()
 
 # Save table with read counts per taxa and sample.
